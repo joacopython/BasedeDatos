e91f6391cb90b52f9376bfae3616594b6ec6aff6
someeday <exequiel.cubillos@uc.cl>
2024-10-10 21:36:59 -0300

    subiendo carpeta ayudantia 6

---
+++ Ayudantía 6/src/config/data.php
@@ -0,0 +1,5 @@
+<?php
+  $user = 'tomas'; // acá va su grupo, ejemplo grupoXX
+  $password = '1234'; // acá la contraseña del grupo
+  $databaseName = 'grupo0'; // acá la base de datos debe llamarse así: grupoXX
+?>
