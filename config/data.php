ca87044386cdae13508d920b2f91b33582d16cee
someeday <exequiel.cubillos@uc.cl>
2024-11-19 22:09:29 -0300

    terminandoooo

--- config/data.php
+++ config/data.php
@@ -1,5 +1,5 @@
 <?php
-  $user = 'grupo57'; // acá va su grupo, ejemplo grupoXX
+  $user = 'grupo57e3'; // acá va su grupo, ejemplo grupoXX
   $password = 'exjofa'; // acá la contraseña del grupo
-  $databaseName = 'grupo57'; // acá la base de datos debe llamarse así: grupoXX
+  $databaseName = 'grupo57e3'; // acá la base de datos debe llamarse así: grupoXX
 ?>
